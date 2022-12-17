a2d0bdcbde0f63d53e50822ed023bf9387f35ed4
giddyPwild <giddygbenga4@gmail.com>
2022-12-17 16:10:33 +0100

    recent12

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,12 @@ function App() {
   };
 
   const fetchData2 = async () => {
-    const url = await fetch("http://www.omdbapi.com/?apikey=ab036132&s=disney",
-    {
-      method: "GET",
-    }
+    const url = await fetch("https://www.omdbapi.com/?apikey=ab036132&s=disney",
+      {
+        method: "GET",
+      }
     )
-    ;
+      ;
     try {
       const response = await fetch(url);
       const json = await response.json();
@@ -48,7 +48,7 @@ function App() {
     // clean up 
     fetchData();
     fetchData2();
-  
+
   }, []);
 
   return (
@@ -70,32 +70,32 @@ function App() {
         <Search />
       </div>
       <div className={`w-full pl-[28px] md:pl-[67px]`}>
-        
-          <div className='md:h-[349px] h-[240px]'>
-            <span className='text-[18px] md:text-[24px] font-400 leading-[31px]'>Marvel</span>
-            <div className='flex flex-row gap-[13px] mt-[18px] overflow-auto'>
-              {
-                marvelMovie.slice(0, 7).map((movie, i) => (
-                  <div key={i} className='flex flex-row p-[10px] gap-[10px] w-[200px] md:w-[300px] h-[200px] md:h-[300px] rounded-[12px] bg-black'>
-                    <span className='w-[190px] md:w-[280px] h-[190px] md:h-[280px] font-[400] text-[18px] md:text-[24px] text-white leading-[31px] flex items-center justify-center text-center grow self-stretch'>{movie.Title}</span>
-                  </div>
-                ))
-              }
-            </div>
+
+        <div className='md:h-[349px] h-[240px]'>
+          <span className='text-[18px] md:text-[24px] font-400 leading-[31px]'>Marvel</span>
+          <div className='flex flex-row gap-[13px] mt-[18px] overflow-auto'>
+            {
+              marvelMovie.slice(0, 7).map((movie, i) => (
+                <div key={i} className='flex flex-row p-[10px] gap-[10px] w-[200px] md:w-[300px] h-[200px] md:h-[300px] rounded-[12px] bg-black'>
+                  <span className='w-[190px] md:w-[280px] h-[190px] md:h-[280px] font-[400] text-[18px] md:text-[24px] text-white leading-[31px] flex items-center justify-center text-center grow self-stretch'>{movie.Title}</span>
+                </div>
+              ))
+            }
           </div>
-          <div className='h-[349px] mt-[20px] md:my-[48px] mb-[-50px]'>
-            <span className='text-[18px] md:text-[24px] font-400 leading-[31px]'>DC</span>
-            <div className='flex flex-row gap-[13px] mt-[18px] overflow-auto'>
-              {
-                disneyMovie.slice(0, 7).map((movie, i) => (
-                  <div key={i} className='flex flex-row p-[10px] gap-[10px] w-[200px] md:w-[300px] h-[200px] md:h-[300px] rounded-[12px] bg-black'>
-                    <span className='w-[190px] md:w-[280px] h-[190px] md:h-[280px] font-[400] text-[18px] md:text-[24px] text-white leading-[31px] flex items-center justify-center text-center grow self-stretch'>{movie.Title}</span>
-                  </div>
-                ))
-              }
-            </div>
+        </div>
+        <div className='h-[349px] mt-[20px] md:my-[48px] mb-[-50px]'>
+          <span className='text-[18px] md:text-[24px] font-400 leading-[31px]'>DC</span>
+          <div className='flex flex-row gap-[13px] mt-[18px] overflow-auto'>
+            {
+              disneyMovie.slice(0, 7).map((movie, i) => (
+                <div key={i} className='flex flex-row p-[10px] gap-[10px] w-[200px] md:w-[300px] h-[200px] md:h-[300px] rounded-[12px] bg-black'>
+                  <span className='w-[190px] md:w-[280px] h-[190px] md:h-[280px] font-[400] text-[18px] md:text-[24px] text-white leading-[31px] flex items-center justify-center text-center grow self-stretch'>{movie.Title}</span>
+                </div>
+              ))
+            }
           </div>
-        
+        </div>
+
       </div>
 
     </div>
